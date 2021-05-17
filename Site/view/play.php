c8be0114e6f1bb38e07ede63f342fe96f55d8e3e
jeromejj <jerome.jaquemet@gmail.com>
2021-05-17 11:53:45 +0200

    Game timer and player added

--- Site/view/play.php
+++ Site/view/play.php
@@ -14,15 +14,20 @@ $title = "SuperBlinder - Jeu";
 
   ><div style="width: 50%; height: 100%;">
     <div class="game-container">
+
       <div class="video-container">
-        <video style="width: 100%; max-height: 400px">
+        <video id="audio" style="width: 0">
+         <source src="/view/content/vids/audio.mp4" type="video/mp4">
+        </video>
+        <video id="video" style="visibility: hidden; width: 100%; max-height: 400px" muted>
          <source src="/view/content/vids/audio.mp4" type="video/mp4">
         </video>
       </div>
 
+      <h4 id="remaining-time" style="text-align: center"></h4>
 
-      <div class="selection" style="margin-bottom: 30px; visibility: hidden">
-        <div><div style="">Star Wars</div></div
+      <div id="selection" style="margin-bottom: 30px; visibility: hidden">
+        <div><div>Star Wars</div></div
         ><div><div>Narnia</div></div
         ><div><div>Seigneur des anneaux</div></div>
         <br>
@@ -38,9 +43,11 @@ $title = "SuperBlinder - Jeu";
   ><div style="width: 36%; height: 100%">
     <div class="game-side">
       <div>
-        <h3>Code de la partie</h3>
+        <h4>Code de la partie</h4>
         <p>sfhdgncncn</p>
         <p>1 bonne réponse sur 2 - 50%</p>
+
+        <h4>Score</h4>
       </div
 
       ><div class="game-list">
@@ -54,8 +61,33 @@ $title = "SuperBlinder - Jeu";
 <script>
   document.getElementsByTagName("body")[0].onclick = function()
   {
-    document.getElementsByTagName("video")[0].play();
-    document.getElementsByClassName("selection")[0].style.visibility = "visible";
+    document.getElementsByTagName("body")[0].onclick = "";
+    document.getElementById("audio").play();
+    document.getElementById("selection").style.visibility = "visible";
+
+    let timeout = 3;
+    document.getElementById("remaining-time").textContent = timeout+" secondes restantes";
+
+    let timer = setInterval(function() {
+      timeout-=1;
+
+      if(timeout > 1){
+        document.getElementById("remaining-time").textContent = timeout+" secondes restantes";
+      }
+      else if(timeout >= 0)
+      {
+        document.getElementById("remaining-time").textContent = timeout+" seconde restante";
+      }
+
+      if(timeout == -1){
+        document.getElementById("remaining-time").textContent = "Fin";
+        document.getElementById("selection").style.visibility = "hidden";
+        document.getElementById("video").style.visibility = "visible";
+        document.getElementById("video").play();
+        clearInterval(timer);
+        return;
+      }
+    }, 1000);
   }
 </script>
 
