fc1265b2a050782736ae6f0df45715122bf3e280
jeromejj <jerome.jaquemet@gmail.com>
2021-06-01 15:59:15 +0200

    Added user creation script

---
+++ Documents/createUser.sql
@@ -0,0 +1,3 @@
+CREATE USER 'tpi21blind_db'@'localhost' IDENTIFIED BY 'ROOTtoor8$';
+GRANT USAGE ON *.* TO 'tpi21blind_db'@'localhost';
+GRANT EXECUTE, SELECT, INSERT  ON `tpi21blind\_db`.* TO 'tpi21blind_db'@'localhost';
