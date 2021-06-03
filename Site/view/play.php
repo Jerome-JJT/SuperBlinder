828ef05a4a05cc88481aa9c3c9fc5d8be4c8c97f
jeromejj <jerome.jaquemet@gmail.com>
2021-06-03 09:26:01 +0200

    Display video answer even for good answer, add skip button

--- Site/view/play.php
+++ Site/view/play.php
@@ -17,6 +17,10 @@ $tracksDir = "/tracks/";
 
 ?>
 
+<script>
+  var timeout = 10;
+</script>
+
 <div class="page-center generator-tricols" style="height: 600px">
 
   <div style="width: 10%"></div
@@ -32,7 +36,7 @@ $tracksDir = "/tracks/";
         <video id="audio" style="width: 0">
          <source src="<?=$tracksDir.$gameUrl?>" type="video/mp4">
         </video>
-        <video id="video" style="visibility: hidden; width: 100%; max-height: 400px" muted>
+        <video id="video" style="visibility: hidden; width: 100%; max-height: 350px" muted>
          <source src="<?=$tracksDir.$gameUrl?>" type="video/mp4">
         </video>
       </div>
@@ -41,13 +45,20 @@ $tracksDir = "/tracks/";
 
       <div id="selection" style="margin-bottom: 30px; visibility: hidden">
         <?php foreach($answerList[0] as $answer):
-          ?><div onclick="window.location='/?page=play&answer=<?=$answer["key"]?>'"><div><?=$answer["value"]?></div></div><?php
+          ?><div onclick="document.getElementById('answer').value='<?=$answer["key"]?>';hideOptions();timeout=1;"><div><?=$answer["value"]?></div></div><?php
           endforeach ?>
           <br>
         <?php foreach($answerList[1] as $answer):
-          ?><div onclick="window.location='/?page=play&answer=<?=$answer["key"]?>'"><div><?= $answer["value"] ?></div></div><?php
+          ?><div onclick="document.getElementById('answer').value='<?=$answer["key"]?>';hideOptions();timeout=1;"><div><?= $answer["value"] ?></div></div><?php
           endforeach ?>
       </div>
+
+      <form id="answerForm" method="GET" style="text-align: center">
+        <input type="hidden" name="page" value="play">
+        <input id="answer" type="hidden" name="answer" value="-1">
+
+        <button type="submit">Passer</button>
+      </form>
     </div>
   </div
 
@@ -75,6 +86,11 @@ $tracksDir = "/tracks/";
 </div>
 
 <script>
+  function hideOptions()
+  {
+    document.getElementById("selection").style.visibility = "hidden";
+  }
+
   document.getElementsByTagName("body")[0].onclick = function()
   {
     //Hide indication text
@@ -86,7 +102,6 @@ $tracksDir = "/tracks/";
     document.getElementById("selection").style.visibility = "visible";
 
     //Display remaining time
-    let timeout = 10;
     document.getElementById("remaining-time").textContent = timeout+" secondes restantes";
 
     let timer = setInterval(function() {
@@ -104,14 +119,14 @@ $tracksDir = "/tracks/";
       //If timer finished, hide options and display answer's video
       if(timeout == -1){
         document.getElementById("remaining-time").textContent = "Fin";
-        document.getElementById("selection").style.visibility = "hidden";
+        hideOptions();
         document.getElementById("video").style.visibility = "visible";
         document.getElementById("video").play();
       }
 
       //After 5 seconds, skips by sending wrong answer
       if(timeout < -5){
-        window.location = "/?page=play&answer=-1";
+        document.getElementById("answerForm").submit();
       }
     }, 1000);
   }
